c0a30eaca7bf9b8094a79879bbdd2e08ce0ffcad
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2025-03-19 15:04:25 +0100

    ch: prepared for dev databases

--- BigData-Integration/src/db_extractor.py
+++ BigData-Integration/src/db_extractor.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 from llm import query_groq
-from examples import findDB_example1, findDB_example2, findDB_example3
+from examples import findDB_example1DEV, findDB_example2DEV, findDB_example3DEV
 
 ### --------- ###
 prv_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -15,41 +15,33 @@ from ansi_colors import *
 def get_databases_from_query(query: str) -> str:
 
     """
-    Function to extract the top 10 most relevant databases from query.
+    Function to extract the top 3 most relevant databases from query.
     
     Args:
         query (str): Natural language query.
         
     Returns:
-        str: top 10 most relevant databases (separated by comma).
+        str: top 3 most relevant databases (separated by comma).
     """
 
     system_prompt = """(forget the previous answers) You are an agent in the field of big data integration. 
-    Given a natural language query, identify the top 10 most relevant databases from the following list. 
-    (RESPOND ONLY WITH THE NAMEs OF THE DATABASES)
+    Given a natural language query, identify the top 3 most relevant databases from the following list. 
+    (RESPOND ONLY WITH THE NAMES OF THE DATABASES)
 
-    - DATABASES: ['european_football_1', 'sales_in_weather', 'craftbeer', 'soccer_2016', 'restaurant',
-    'movie', 'olympics', 'language_corpus', 'app_store', 'sales', 'video_games', 'image_and_language',
-    'software_company', 'authors', 'movies_4', 'social_media', 'human_resources', 'regional_sales',
-    'computer_student', 'works_cycles', 'food_inspection_2', 'citeseer', 'bike_share_1', 'law_episode',
-    'cs_semester', 'legislator', 'world', 'cookbook', 'university', 'books', 'shipping', 'food_inspection',
-    'movie_platform', 'shakespeare', 'book_publishing_company', 'car_retails', 'mental_health_survey',
-    'hockey', 'music_platform_2', 'address', 'menu', 'professional_basketball', 'cars', 'synthea',
-    'genes', 'retails', 'talkingdata', 'beer_factory', 'chicago_crime', 'mondial_geo', 'student_loan',
-    'codebase_comments', 'retail_world', 'music_tracker', 'disney', 'college_completion', 'ice_hockey_draft',
-    'world_development_indicators', 'airline', 'retail_complains', 'trains', 'public_review_platform',
-    'donor', 'coinmarketcap', 'simpson_episodes', 'movie_3', 'shooting', 'superstore', 'movielens']
+    - DATABASES: ['debit_card_specializing', 'financial', 'formula_1', 'california_schools',
+                  'card_games', 'european_football_2', 'thrombosis_prediction', 'toxicology', 'student_club',
+                  'superhero', 'codebase_community']
 
     ---
-    Example 1: """ + findDB_example1.__str__() + """
+    Example 1: """ + findDB_example1DEV.__str__() + """
     ---
-    Example 2: """ + findDB_example2.__str__() + """
+    Example 2: """ + findDB_example2DEV.__str__() + """
     ---
-    Example 3: """ + findDB_example3.__str__() + """
+    Example 3: """ + findDB_example3DEV.__str__() + """
     
     """
 
-    content = f"Provide me the 10 most relevant databases based on this QUERY: {query}"
+    content = f"Provide me the 3 most relevant databases based on this QUERY: {query}"
 
 
     return query_groq(messages=[
@@ -88,7 +80,10 @@ if __name__ == '__main__':
             question = query_block["question"]
             sql = query_block["SQL"]
             response = get_databases_from_query(question)
-
+            
+            if "question_id" in query_block:
+                q_id = query_block["question_id"]
+                file.write(f"{space}DBid: {q_id}\n")
 
             file.write(f"{space}DBid: {db_id}\n")
             file.write(f"{space}QUESTION: {question}\n")
