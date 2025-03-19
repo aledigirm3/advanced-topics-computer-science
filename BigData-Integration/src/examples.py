a262e0763d97e2e0e8baafca0ebe04d18830dbc7
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2025-03-19 15:04:41 +0100

    ch: examples for dev databases

--- BigData-Integration/src/examples.py
+++ BigData-Integration/src/examples.py
@@ -28,3 +28,19 @@ findDB_example3 = TaskExample(
     query="When was the project with the highest quantity went live on the site? Indicate the grade level for which the project materials are intended.",
     answer = "software_company, cs_semester, computer_student, university, books, book_publishing_company, codebase_comments, authors, regional_sales"
 )
+
+
+findDB_example1DEV = TaskExample(
+    query="Who is the top spending customer and how much is the average price per single item purchased by this customer? What currency was being used?",
+    answer="financial, debit_card_specializing, card_games"
+)
+
+findDB_example2DEV = TaskExample(
+    query="Which post has the highest score? Please give its id and title's name",
+    answer="codebase_community, financial, student_club"
+)
+
+findDB_example3DEV = TaskExample(
+    query="What is the educational level name for the schools with Breakfast Provision 2 in county code 37? Indicate the name of the school.",
+    answer="california_schools, student_club, financial"
+)
