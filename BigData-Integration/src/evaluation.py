8cfda63a342d929b43ba434b51ed24a256c4f4a2
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2025-03-31 01:26:09 +0200

    ft: att_match_table_eval

--- BigData-Integration/src/evaluation.py
+++ BigData-Integration/src/evaluation.py
@@ -1,4 +1,4 @@
-import os
+import re
 import sys
 from data_manipulation import get_db_dict_mapping, get_tables_from_SQLquery, get_entry_from_llmResponse
 import paths
@@ -63,10 +63,83 @@ def table_match_eval(filename: str):
     print(f"{GREEN}PRECISION: {RESET}{precision:.2f}")
     print(f"{GREEN}RECALL: {RESET}{recall:.2f}")
     print(f"{GREEN}F1-score: {RESET}{f1_score:.2f}")
+    
+def att_table_match_eval(filename: str):
+    
+    original2name, name2original = get_db_dict_mapping(paths.DEV + 'dev_tables.json')
+    
+    data = get_entry_from_llmResponse(filename, withAttributes=True)
+    
+    table_tp = 0
+    table_fp = 0
+    table_fn = 0
+    
+    att_tp = 0
+    att_fp = 0
+    att_fn = 0
+    
+    n_ist = 0
+    
+    for qid, entry in data.items():
+        n_ist += 1
+        print(f"{RED}{qid}{RESET}")
+        truth_tables = get_tables_from_SQLquery(entry['SQL'])
+        truth_attributes = entry['attributes'].split(",")
+        truth_attributes =[item.strip() for item in truth_attributes]
+        
+        database, _, result = entry['llmRESPONSE'].partition(":")
+        
+        database = database.strip()
+        
+        predicted_att_tables_list = re.findall(r'\b[\w\s\-]+\s*\(.*?\)', result)
+        predicted_att_tables = [item.strip() for item in predicted_att_tables_list]
+        
+        for att_table_str in predicted_att_tables:
+            
+            att_table = re.match(r'\s*([\w\s\-]+)\s*\((.*?)\)', att_table_str)
+            table = att_table.group(1)
+            attributes = [att.strip() for att in att_table.group(2).split(',')]
+            
+            if name2original[database][table] in truth_tables:
+
+                table_tp += 1
+                truth_tables.remove(name2original[database][table])
+            else:
+                table_fp += 1
+                
+            for att in attributes:
+                if att in truth_attributes:
+                    att_tp += 1
+                    truth_attributes.remove(att)
+                else:
+                    att_fp += 1
+                    
+        table_fn += len(truth_tables)
+        att_fn += len(truth_attributes)
+            
+
+    precision = table_tp / (table_tp + table_fp) if (table_tp + table_fp) > 0 else 0
+    recall = table_tp / (table_tp + table_fn) if (table_tp + table_fn) > 0 else 0
+    f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+    
+    print(f"{CYAN}*===Table evaluation===*{RESET}")
+    print(f"{GREEN}PRECISION: {RESET}{precision:.2f}")
+    print(f"{GREEN}RECALL: {RESET}{recall:.2f}")
+    print(f"{GREEN}F1-score: {RESET}{f1_score:.2f}")
+    print('\n')
+    precision = att_tp / (att_tp + att_fp) if (att_tp + att_fp) > 0 else 0
+    recall = att_tp / (att_tp + att_fn) if (att_tp + att_fn) > 0 else 0
+    f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+    
+    print(f"{CYAN}*===Attributes evaluation===*{RESET}")
+    print(f"{GREEN}PRECISION: {RESET}{precision:.2f}")
+    print(f"{GREEN}RECALL: {RESET}{recall:.2f}")
+    print(f"{GREEN}F1-score: {RESET}{f1_score:.2f}")
+    print(n_ist)
 
 
 if __name__ == '__main__':
 
-    filename = paths.LLM_RESPONSE + "match_tables.txt"
+    filename = paths.LLM_RESPONSE + "att_match_tables.txt"
 
-    table_match_eval(filename)
+    att_table_match_eval(filename)
