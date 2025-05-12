e9239b61e8f730f166e94a5d847aff723c4554cc
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2025-05-12 19:23:08 +0200

    ft: summarization

---
+++ NeuralTable-Representations/src/summarization_udf.py
@@ -0,0 +1,65 @@
+import os
+import pandas as pd
+import glob
+from llm import query_groq
+
+def summarize(descriptions: list[str]) -> str:
+
+    """
+    Make summarization of entity descriptions.
+    
+    Args:
+        query (list[str]): Natural language query.
+        
+    Returns:
+        str: relevant tables.
+    """
+
+    system_prompt = """
+    You are a helpful assistant skilled in summarizing subjective descriptions provided by multiple individuals. 
+
+    Your task is to read a numbered list of brief descriptions, each expressing a personal impression or opinion about a single, unnamed entity. The entity may be anything — such as a product, a book, a film, a public figure, or something else entirely — and is intentionally not specified. 
+
+    Based solely on the content of the descriptions, identify common themes, sentiments, and relevant insights. Then, write a coherent, concise summary that fairly represents the overall perspectives given, while acknowledging any diversity of opinion when present.
+
+    Do not assume any specific type of entity. Infer meaning purely from the input. The summary should be neutral, informative, and human-like in tone.
+
+    """
+    content = "\n".join(f"{i+1} {desc}" for i, desc in enumerate(descriptions))
+    content = f"""
+    Please summarize the following personal descriptions:
+
+    {content}
+    """
+
+
+    return query_groq(messages=[
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": content
+        }
+    ])
+
+
+folder = '../GT/*.csv'
+
+# All tables
+files = glob.glob(folder)
+with open('../llmResponse.txt', "a", encoding="utf-8") as file:
+    for file in files:
+        filename = os.path.splitext(os.path.basename(file))[0]
+        print(filename)
+        
+        df = pd.read_csv(file)
+        
+        descriptions = df['description'].tolist()
+        #summarization = summarize(descriptions)
+        
+        file.write(f"Table: {filename}\n")
+        file.write(f"llmRESPONSE: {'# SUMMARIZATION #'}\n\n")
+        file.flush()
+    
